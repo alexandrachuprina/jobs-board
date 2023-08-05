1ab45b9a57a4da0dea81a2d1560149349abdf1d0
shplvk <114808997+shplvk@users.noreply.github.com>
2023-08-05 15:28:20 +0300

    fix: base url

--- src/modules/api/api.js
+++ src/modules/api/api.js
@@ -1,6 +1,6 @@
 import ENDPOINTS from "modules/api/endpoints";
 
-const BASE_URL = "https://5f7998dbe402340016f9321f.mockapi.io/providers";
+const BASE_URL = "https://5f7998dbe402340016f9321f.mockapi.io";
 
 class Api {
   constructor(baseUrl, endpoints) {
@@ -11,11 +11,11 @@ class Api {
   async generateRequest(endpoint, data) {
     const { uri, method } = this.endpoints[endpoint];
 
-    return fetch(`${this.baseUrl}${uri}`, { method });
+    return fetch(`${this.baseUrl}${uri}`, { method, body: data });
   }
 
-  async fetch(endpoint) {
-    const response = await this.generateRequest(endpoint);
+  async fetch(endpoint, data) {
+    const response = await this.generateRequest(endpoint, data);
 
     return response.json()
   }
